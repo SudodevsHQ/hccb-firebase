49df626a1c50d92ebd28598a32f0e98d653c99be
AayushDongre <aayushdongre4@gmail.com>
2020-08-24 22:19:55 +0530

    chore: setup pre-commit hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Coke LOL</h1>
+      <h1>Coke Lmfo</h1>
+
+      <p>okay description</p>
     </div>
   );
 }
