d8c880df6e306b2f7fd5f53d37c7c3ceedec349a
source <anurag@sudodevs.com>
2020-08-30 18:50:30 +0530

    feat: added link building logic and retrieval

---
+++ src/services/invite/logic.ts
@@ -0,0 +1,16 @@
+import { AES, enc } from 'crypto-js';
+import { firebase } from '../../config/firebaseConfig';
+
+export function generateLinkText(email: string): string {
+  const link = AES.encrypt(email, email);
+  return link.toString();
+}
+
+export function decryptLinkText(url: string, email: string): string {
+  const link = AES.decrypt(url, email);
+  return link.toString(enc.Utf8);
+}
+
+export function isInviteValid(quizId: string, email: string): string {
+  return 'todo';
+}
