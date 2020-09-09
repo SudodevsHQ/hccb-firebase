e215b05a96b2a6178bcc8a1f50e9d56e041376d7
therexone <renerjake@gmail.com>
2020-09-09 22:12:50 +0530

    chore: added state to LapOneResult

--- src/modules/Module-1/Lap-1/LapOneResult.tsx
+++ src/modules/Module-1/Lap-1/LapOneResult.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Result from '../../../components/result/result';
+import { useSelector } from 'react-redux';
+import { ReduxStore } from '../../../interfaces/reduxStore';
+import { moduleOneResultData } from '../data';
 
 const LapOneResult: React.FC = () => {
+  const savings = useSelector(
+    (state: ReduxStore) => state.moduleOne.lapOne.amount,
+  );
+
   return (
     <Result
       lapNumber={1}
       title={'Budget Management'}
-      qualities={['Accountable and Responsible']}
+      qualities={moduleOneResultData.lapOne(savings)}
       remark="You saved ₹6000"
       nextPath="/module/1/lap/2"
     />
