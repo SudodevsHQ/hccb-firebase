3e120e86b296fbfc19ceac998bb3636b067f1b88
therexone <renerjake@gmail.com>
2020-09-09 20:52:43 +0530

    fix: audio now autoplays after video page

--- src/components/BgAudioPlayer/BgAudioPlayer.tsx
+++ src/components/BgAudioPlayer/BgAudioPlayer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useSound from 'use-sound';
 import { Icon } from '@iconify/react';
 import soundOnSolid from '@iconify/icons-ant-design/sound-fill';
@@ -8,15 +8,25 @@ import styles from './BgAudioPlayer.module.scss';
 import { ReduxStore } from '../../interfaces/reduxStore';
 import { useSelector, useDispatch } from 'react-redux';
 import { setAudioState } from '../../redux/actions/miscActions';
+import { useLocation } from 'react-router-dom';
 
 const BgAudioPlayer: React.FC = () => {
   const isAudioPlaying = useSelector((state: ReduxStore) => state.misc.playing);
   const dispatch = useDispatch();
-
+  const location = useLocation();
   const [play, { pause }] = useSound('/static/audio/quiz-bg-music.mp3', {
     interrupt: true,
     loop: true,
   });
+  useEffect(() => {
+    if (
+      location.pathname.match(/\/module\/\d\/introduction/)?.length !==
+      undefined
+    ) {
+      dispatch(setAudioState(true));
+      play();
+    }
+  }, [dispatch, location.pathname, play]);
 
   return (
     <div
