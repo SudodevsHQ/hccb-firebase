75827729608c2000e801c59156fa18403157fafd
Aayush Dongre <44211899+AayushDongre@users.noreply.github.com>
2020-10-15 10:49:46 +0530

    feat: add login page (#28)
    * feat: add local video
    
    * feat: add server validation error and loading screen
    
    * feat: post result on module completion
    
    * fix: pdf file name and download result path
    
    * feat: add minimal login page with logic
    
    * fix: module-5 routing and login page bugs
    
    Co-authored-by: therexone <renerjake@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import { pageTransitions } from './util/rrtConfig';
 
-import TopBar from './components/TopBar/TopBar';
-import useCheckAuth from './hooks/useCheckAuth';
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps, useLocation } from 'react-router-dom';
 import ModuleOne from './modules/Module-1/ModuleOne';
+import LoginPage from './Login';
 
 const App: React.FunctionComponent = () => {
-  const { isValid, email, quizID, isLoading } = useCheckAuth();
-
-  if (isLoading) return <div>loading</div>;
-  if (!isValid) return <div>no auth</div>;
-
+  const location = useLocation();
+  const path = location.pathname.split('/');
+  const employee_id = path[3];
+  const quizID = path[2];
   return (
     <>
-      <TopBar />
       <AnimatedSwitch
         {...pageTransitions}
         //eslint-disable-next-line
@@ -27,7 +24,13 @@ const App: React.FunctionComponent = () => {
         })}
         className="switch-wrapper">
         <AnimatedRoute
-          path={`/quiz/${quizID}/${email}`}
+          path={`/quiz/${quizID}`}
+          component={LoginPage}
+          exact={true}
+        />
+
+        <AnimatedRoute
+          path={`/quiz/${quizID}/${employee_id}`}
           render={({ match: { path } }: RouteComponentProps) => (
             <>
               <AnimatedRoute path={`${path}/module/1`} component={ModuleOne} />
--- src/modules/Module-1/ModuleOneConclusion/ModuleOneConclusion.tsx
+++ src/modules/Module-1/ModuleOneConclusion/ModuleOneConclusion.tsx
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useRouteMatch } from 'react-router-dom';
 import Conclusion from '../../../components/Conclusion/Conclusion';
+import { ReduxStore } from '../../../interfaces/reduxStore';
 
 const ModuleOneConclusion: React.FC = () => {
+  const moduleResultData = useSelector(
+    (state: ReduxStore) => state.moduleOne.moduleResult,
+  );
+  const [postResponse, setPostResponse] = useState(undefined);
+  const { url } = useRouteMatch();
+
+  useEffect(() => {
+    const commitResult = async () => {
+      const id = url.split('/')[2];
+      const email = url.split('/')[3];
+
+      console.log(
+        moduleResultData.map((lapResult) => ({
+          ...lapResult,
+          quiz_id: `quiz/${id}`,
+        })),
+      );
+      const res = await fetch(
+        `${process.env.REACT_APP_BASE_URL}/quiz/add-data`,
+        {
+          method: 'POST',
+          mode: 'cors',
+          credentials: 'same-origin',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            data: moduleResultData.map((lapResult) => ({
+              ...lapResult,
+              key: `quiz/${id}`,
+              email,
+            })),
+          }),
+        },
+      );
+      const json = await res.json();
+      console.log(json);
+      setPostResponse(json);
+      console.log(postResponse);
+    };
+
+    commitResult();
+  }, []);
+
   return (
     <Conclusion
       title="Good Job!"
